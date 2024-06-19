6a75a19c2be43374554a67247036287ca98e5984
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-06-19 19:14:41 +0200

    ✨ feat(log): small logging interface

---
+++ src/log.rs
@@ -0,0 +1,49 @@
+// Q: Why not use an existing solution?
+// A: I need something extremely simple, and having a logging library that depends
+//    on 7 others and provides 1000 features is extremely overkill for the scope of this project.
+
+pub enum LogType {
+    Info,
+    Warn,
+    Error,
+    Success,
+}
+
+pub fn log(_type: LogType, ctx: &str, message: &str) {
+    let context_color = 107;
+    let color = match _type {
+        LogType::Info => 104,
+        LogType::Warn => 103,
+        LogType::Error => 101,
+        LogType::Success => 102,
+    };
+
+    let prefix_type = match _type {
+        LogType::Info => "inf",
+        LogType::Warn => "wrn",
+        LogType::Error => "err",
+        LogType::Success => "suc",
+    };
+
+    let prefix = format!("\x1b[1;37;{}m  {}  \x1b[0m", color, prefix_type);
+    let ctx = format!("\x1b[1;37;{}m  {}  \x1b[0m", context_color, ctx);
+    let message = format!("\x1b[37;{}m   \x1b[0m {}", color, message);
+
+    println!("{}{}{}", prefix, ctx, message);
+}
+
+pub fn info(ctx: &str, message: &str) {
+    log(LogType::Info, ctx, message);
+}
+
+pub fn warn(ctx: &str, message: &str) {
+    log(LogType::Warn, ctx, message);
+}
+
+pub fn error(ctx: &str, message: &str) {
+    log(LogType::Error, ctx, message);
+}
+
+pub fn success(ctx: &str, message: &str) {
+    log(LogType::Success, ctx, message);
+}
